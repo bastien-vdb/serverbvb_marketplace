c8193b91d443ca200170644f980b8e2de42cc964
bastien-vdb <bastien.deboisrolin@gmail.com>
2023-02-26 09:10:14 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const Moralis = require('moralis').default;
+
+const express = require('express');
+const cors = require('cors');
+
+const { EvmChain } = require('@moralisweb3/common-evm-utils');
+
+const app = express();
+const port = process.env.PORT || 3007;
+
+// allow access to React app domain
+app.use(
+    cors({
+        origin: '*',
+        credentials: true,
+    })
+);
+
+const MORALIS_API_KEY = 'B23o2RvkbaPp28e6TardbHQFyed6aXS7fBMECKjLUD7UCQvrGZeg8FW52Ac8gHSF';
+
+app.get('/balances/:address', async (req, res) => {
+    try {
+
+        const address = req.params.address;
+
+        const chain = EvmChain.GOERLI;
+
+        const data = await Moralis.EvmApi.nft.getWalletNFTs({
+            address,
+            chain,
+            normalizeMetadata: true,
+        });
+        res.json(data);
+        console.log(data?.result);
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+const startServer = async () => {
+    await Moralis.start({
+        apiKey: MORALIS_API_KEY,
+    });
+
+    app.listen(port, () => {
+        console.log(`Server starts on ${port}`);
+    });
+};
+
+startServer();
